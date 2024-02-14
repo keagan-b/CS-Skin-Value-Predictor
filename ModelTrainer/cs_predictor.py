725ed7aac461ede755d1dc175c0323a071f175d3
HelpMeGame <keagan.a.bowman@gmail.com>
2024-02-14 10:09:53 -0500

    regression model

--- ModelTrainer/cs_predictor.py
+++ ModelTrainer/cs_predictor.py
@@ -2,7 +2,9 @@ import os
 
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
+import json
 import keras
+import sqlite3
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
@@ -58,34 +60,27 @@ def train_model():
     conv_4 = layers.Conv2D(filters=128, kernel_size=(4, 4))(pool_3)
     """
 
-    conv_1 = layers.Conv2D(filters=64, kernel_size=(16, 16))(texture_input)
+    conv_1 = layers.Conv2D(filters=16, kernel_size=(16, 16), input_shape=(512, 512, 4))(texture_input)
+    pool_1 = layers.MaxPooling2D((2, 2))(conv_1)
+    # conv_2 = layers.Conv2D(filters=16, kernel_size=(4, 4))(pool_1)
+    # pool_2 = layers.MaxPooling2D((4, 4))(conv_2)
 
-    pool_1 = layers.MaxPooling2D((4, 4))(conv_1)
+    flat = layers.Flatten()(pool_1)
 
-    conv_2 = layers.Conv2D(filters=16, kernel_size=(8, 8))(pool_1)
-    pool_2 = layers.MaxPooling2D((14, 14))(conv_2)
+    norm = layers.Normalization()(flat)
 
-    drop = layers.Dropout(rate=0.01)(pool_2)
+    dense_1 = layers.Dense(units=64)(norm)
 
-    # flatten layer
-    flattened = layers.Flatten()(drop)
+    drop_1 = layers.Dropout(rate=0.05)(dense_1)
 
-    # compress layers
-    dense_1 = layers.Dense(units=64, kernel_regularizer="l2")(flattened)
-
-    dense_2 = layers.Dense(units=32, activation="elu")(dense_1)
-
-    drop_4 = layers.Dropout(rate=0.1)(dense_2)
+    dense_2 = layers.Dense(units=8, activation="relu")(drop_1)
 
     # reshape data for concat
     weapon_input_reshaped = layers.Reshape((-1,))(weapon_input)
     rarity_input_reshaped = layers.Reshape((-1,))(rarity_input)
 
-    # combine texture with weapon type and rarity
-    combined = layers.Concatenate(axis=-1)([weapon_input_reshaped, rarity_input_reshaped, drop_4])
-
-    # create output layer
-    outputs = layers.Dense(units=1, activation="elu")(combined)
+    combined = layers.Concatenate(axis=-1)([weapon_input_reshaped, rarity_input_reshaped, dense_2])
+    outputs = layers.Dense(units=1)(combined)
 
     # compile model
     model = keras.Model(inputs=[texture_input, weapon_input, rarity_input], outputs=outputs,
@@ -94,26 +89,19 @@ def train_model():
 
     # MeanSquaredLogarithmicError
 
+    # Adadelta is good :)
+
+
     model.compile(
-        loss=keras.losses.MeanSquaredLogarithmicError(),
-        optimizer=keras.optimizers.Nadam(),
-        metrics=["accuracy"]
+        loss=keras.losses.MeanSquaredError(),
+        optimizer=keras.optimizers.Adadelta()
     )
 
     # train model
-    history = model.fit(x_train, y_train, batch_size=32, epochs=16)
+    history = model.fit(x_train, y_train, batch_size=64, epochs=64)
 
     # test model
-    test_scores = model.evaluate(x_test, y_test, verbose=2)
-    print(f"Test loss: {test_scores[0]}")
-    print(f"Test accuracy: {test_scores[1]}")
-
-    plt.plot(history.history['accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
+    model.evaluate(x_test, y_test, verbose=2)
 
     plt.plot(history.history['loss'])
     plt.title('model loss')
@@ -126,34 +114,106 @@ def train_model():
     model.save("cs_prediction.keras")
 
 
-def predict():
-    model = keras.models.load_model("cs_prediction.keras")
+def predict(data_name: str, model):
+    # connect to DB to get statistics
+    db = sqlite3.connect("../ItemScraper/Data/skins.db")
 
-    img = keras.utils.img_to_array(
-        keras.utils.load_img(input("File path: "), color_mode="rgba", target_size=(512, 512)))
+    cursor = db.cursor()
+    data = cursor.execute("SELECT skin_price_data, skin_weapon_type, skin_rarity, skin_texture_file FROM skins WHERE skin_data_name = ?", (data_name,)).fetchone()
+    cursor.close()
 
-    weapon_int = int(input("Weapon Int: "))
-    rarity_int = int(input("Rarity Int: "))
+    weapon_int = int(data[1])
+    rarity_int = int(data[2])
+    file_path = data[3]
+
+    price_data = json.loads(data[0])['prices']
+
+    img = tf.convert_to_tensor(
+        keras.utils.img_to_array(
+            keras.utils.load_img(f"../ItemScraper/Textures/{file_path}", color_mode="rgba", target_size=(512, 512))
+        ).reshape((-1, 512, 512, 4))
+    )
+
+    # weapon_int = int(input("Weapon Int: "))
+    # rarity_int = int(input("Rarity Int: "))
 
     weapon_input = tf.constant([weapon_int])
     rarity_input = tf.constant([rarity_int])
 
     prediction = model([img, weapon_input, rarity_input])
 
-    print(np.argmax(prediction))
+    prices = []
+    # gather all prices
+    for price in price_data:
+        # check prices
+        # add parsed price
+        prices.append(float(price[1]))
 
+    prices = sorted(prices)
+
+    print("Actual values:")
+
+    # construct inter-quartile range
+    quarter_size = len(prices) // 4
+    q1 = prices[quarter_size * 1]
+    q2 = prices[quarter_size * 2]
+    q3 = prices[quarter_size * 3]
+    iqr = (q3 - q1) * 1.5
+
+    print(f"Q1: {q1}\nQ2: {q2}\nQ3: {q3}")
+    print(f"Average: {sum(prices) / len(prices)}")
+
+    # get min
+    for price in prices:
+        # ensure price is not an outlier
+        if price < q1 - iqr:
+            continue
+        else:
+            print(f"Min: {price}")
+            break
+
+
+    # get max
+    for price in reversed(prices):
+        # ensure price is not an outlier
+        if price > q3 + iqr:
+            continue
+        else:
+            print(f"Max: {price}")
+            break
+
+
+    print(f"Prediction: {prediction[0][0]}")
+
+
+def load_model():
+    print("Loading model...")
+
+    model = keras.models.load_model("cs_prediction.keras")
+
+    print("Loaded model.")
+
+    while True:
+        data_name = input("Please enter a skin data name, or -1 to exit.\n")
+
+        if data_name == "-1":
+            break
+        else:
+            predict(data_name, model)
 
 def main():
     while True:
         # get user input
-        x = input("Please select an option:\n1.) Train\n2.) Predict\n")
+        x = input("Please select an option:\n1.) Train\n2.) Predict\n3.) Both\n")
 
         if x == "1":
             train_model()
         elif x == "2":
-            predict()
+            load_model()
+        elif x == "3":
+            train_model()
+            load_model()
 
 
 if __name__ == "__main__":
-    # main()
-    train_model()
+    main()
